6a8d5b5159afcb0e96e8e5654993d2700b90224b
Or Rener <orrener2000or@gmail.com>
2025-08-26 14:29:56 -0400

    feat(api/v1/): Add roleUpdaters API CRD.

--- api/v1/roleupdater_types.go
+++ api/v1/roleupdater_types.go
@@ -1,69 +1,45 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// RoleUpdaterSpec defines the desired state of RoleUpdater
-type RoleUpdaterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of RoleUpdater. Edit roleupdater_types.go to remove/update
+type ConfigMapRef struct {
+	Name string `json:"name"`
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	Namespace string `json:"namespace,omitempty"`
 }
 
-// RoleUpdaterStatus defines the observed state of RoleUpdater.
+type RoleUpdaterSpec struct {
+	ConfigMapRef ConfigMapRef `json:"configMapRef"`
+}
+
 type RoleUpdaterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	LastCheckTime    string `json:"lastCheckTime,omitempty"`
+	ClusterVersion   string `json:"clusterVersion,omitempty"`
+	Status           string `json:"status,omitempty"`
+	Message          string `json:"message,omitempty"`
+	ConfigMapPresent bool   `json:"configMapPresent"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
-// RoleUpdater is the Schema for the roleupdaters API
 type RoleUpdater struct {
 	metav1.TypeMeta `json:",inline"`
 
-	// metadata is a standard object metadata
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty,omitzero"`
 
-	// spec defines the desired state of RoleUpdater
 	// +required
 	Spec RoleUpdaterSpec `json:"spec"`
 
-	// status defines the observed state of RoleUpdater
 	// +optional
 	Status RoleUpdaterStatus `json:"status,omitempty,omitzero"`
 }
 
 // +kubebuilder:object:root=true
 
-// RoleUpdaterList contains a list of RoleUpdater
 type RoleUpdaterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
